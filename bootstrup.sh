3703d6f24d807c6e6eed279a81b616022d3c9dce
elyasaflehrer <0548491959l@gmail.com>
2026-02-11 21:30:42 +0200

    support local-registry
    added support to creating local-registy, it's mainly used for CI/CD
    processes

--- bootstrup.sh
+++ bootstrup.sh
@@ -5,7 +5,8 @@ CLUSTER_NAME="kind"
 ARGOCD="true"
 INSTALL_KIND_CLI="false"
 KIND_CLI_PATH="/usr/local/bin"
-
+CREATE_LOCAL_REGISTRY=false
+REGISTRY_PORT=5001
 parse_args(){
   while [[ $# -gt 0 ]]; do
       arg="$1"
@@ -16,6 +17,14 @@ parse_args(){
       case "${arg%%=*}" in
            --install-kind-cli)
               INSTALL_KIND_CLI="${arg#*=}"
+              shift 1
+              ;;
+           --create-local-registry)
+              CREATE_LOCAL_REGISTRY="${arg#*=}"
+              shift 1
+              ;;
+           --registry-port)
+              REGISTRY_PORT="${arg#*=}"
               shift 1
               ;;
            --kind-cli-path)
@@ -48,6 +57,8 @@ function show_help() {
     echo "Options:"
     echo "  --install-kind-cli <value>   installe kind cli (Default false, only true install)"
     echo "  --cluster-name <name>        Specify the cluster name (Default kind)"
+    echo "  --create-local-registry      create local-registy (Default false)"
+    echo "  --registry-port              Specify the local-registry port (Default 5001)"
     echo "  --argocd <value>             installe ArgoCD (Default true, type false to uninstall)"
     echo "  -h, --help                   Show this help message"
     echo
@@ -148,16 +159,60 @@ spec:
 EOF
 }
 
+function create-local-registry(){
+
+  # The content of the function copied from https://kind.sigs.k8s.io/docs/user/local-registry/
+  reg_name='kind-registry'
+  reg_dir="/etc/containerd/certs.d/localhost:${REGISTRY_PORT}"
+  if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+    docker run \
+      -d --restart=always -p "127.0.0.1:${REGISTRY_PORT}:5000" --network bridge --name "${reg_name}" \
+      registry:2
+    else
+      echo "Local-registry already set"
+      return
+  fi
+
+  for node in $(kind get nodes); do
+    docker exec "${node}" mkdir -p "${reg_dir}"
+    cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${reg_dir}/hosts.toml"
+[host."http://${reg_name}:5000"]
+EOF
+  done
+  if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+    docker network connect "kind" "${reg_name}"
+  fi
+
+  cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${REGISTRY_PORT}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+}
+
+
 parse_args "$@"
 
 echo "Deployment starting..."
 echo "Cluster Name: $CLUSTER_NAME"
+echo "Local Registry: $CREATE_LOCAL_REGISTRY"
 echo "ArgoCD: $ARGOCD"
 
 if [[ "$INSTALL_KIND_CLI" == "true" ]]; then
   install_kind_cli
 fi
 install_kind_cluster
+
+if [[ "$CREATE_LOCAL_REGISTRY" == "true" ]]; then
+  echo "creating local-registry"
+  create-local-registry
+fi
 
 if [[ "$ARGOCD" == "true" ]]; then
   echo "installing Argocd"
